300009318a1ceaab753e902a2af2b42d4dc49548
Juliana <jiseong.lee@yonsei.ac.kr>
2025-06-03 18:06:05 +0900

    Initial Commit

---
+++ BuildLifeCycleTests/BuildLifeCycleTests.swift
@@ -0,0 +1,17 @@
+//
+//  BuildLifeCycleTests.swift
+//  BuildLifeCycleTests
+//
+//  Created by Juliana Lee on 6/3/25.
+//
+
+import Testing
+@testable import BuildLifeCycle
+
+struct BuildLifeCycleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
